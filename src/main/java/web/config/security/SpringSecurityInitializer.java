6633bb90835749da5dc71dcf4942c46efcac1e9f
Li Maxim <FrenkLee365@gmail.com>
2021-07-15 20:06:31 +0900

    Delete of config classes

--- src/main/java/web/config/security/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package web.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
